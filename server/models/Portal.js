e77bea3145e1edd206f4f75a57fd725df7d0ba2f
Cailin Li <cxl2467@rit.edu>
2017-04-27 22:50:09 -0400

    Create Portal model

---
+++ server/models/Portal.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const _ = require('underscore');
+
+let PortalModel = {};
+
+const convertId = mongoose.Types.ObjectId;
+const setName = (name) => _.escape(name).trim();
+
+const PortalSchema = new mongoose.Schema({
+  label: {
+    type: String,
+    required: true,
+    trim: true,
+    set: setName,
+  },
+  titles: {
+    type: [String],
+    required: true,
+  },
+  skills: {
+    type: [String],
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.ObjectId,
+    required: true,
+    ref: 'Account',
+  },
+	blurb: {
+		type: String
+	},
+  color: {
+    type: String,
+    default: '#A8444D',
+  },
+});
+
+// DomoSchema.statics.toAPI = (doc) => ({
+//   name: doc.name,
+//   age: doc.age,
+// });
+//
+// DomoSchema.statics.findByOwner = (ownerId, callback) => {
+//   const search = {
+//     owner: convertId(ownerId),
+//   };
+//   return DomoModel.find(search).select('name age faveFood').exec(callback);
+// };
+//
+// DomoSchema.statics.searchByName = (name, callback) => {
+//   const search = {
+//     name: { $regex: name, $options: 'i' },
+//   };
+//
+//   return DomoModel.find(search).select('name age faveFood').exec(callback);
+// };
+
+PortalModel = mongoose.model('Portal', PortalSchema);
+
+module.exports.PortalModel = PortalModel;
+module.exports.PortalSchema = PortalSchema;
